44c52d1fef5e85763a96d5d9c3b4856bc3589eb1
Greg J <jackson@itp.unibe.ch>
2021-02-19 21:13:10 -0800

    19.2.21 -- issue in r_scan_Egit statusgit status

--- nu/main.c
+++ nu/main.c
@@ -18,8 +18,8 @@ double x_max = 100.; // soft-gluon approx. ?
 FILE *in;
 FILE *out;
 
-#define SQR(x) x*x
-#define SGN(x) (double) ((x>0)-(x<0))
+void Z_scan_g();
+void r_scan_E(double(*)(double,double),double(*)(double));
 
 /*--------------------------------------------------------------------*/
 
@@ -55,8 +55,6 @@ void Zpc_FCEL(Fc,E,dsig,phi,params,res)
   /*
    *  Z_pc moment w/ FCEL effect
    */
-  if (fabs(Fc)<tol) {*res=1.; return;} // no mods.
-  if (Fc<0) { printf("FCEG still to be implemented\n"); return; }
 
   double as      = ((double *)params)[0]; // coupling/(2*pi)
   double Qs_M    = ((double *)params)[1]; // Qs/M
@@ -68,8 +66,8 @@ void Zpc_FCEL(Fc,E,dsig,phi,params,res)
   double _inner(double xF, void *params_inner) {
     // xF ~ x1-x2 (?)
     double x  = ((double *)params_inner)[0];
-    double Ep = E*(1.+x)/xF;
-    return dsig(Ep,xF)*phi(Ep)/(xF*phi(E));
+    double Ep = fabs(E)*(1.+x)/xF;
+    return dsig(Ep,xF)*phi(Ep)/(xF*phi(fabs(E)));
   };
 
   double _outer(double x, void *params_outer) {
@@ -85,40 +83,31 @@ void Zpc_FCEL(Fc,E,dsig,phi,params,res)
     return res_inner*P;
   };
 
+  double inn[1] = {0.}; // to calc. Zpc w/o FCEL
+  integrator(1e-8,1.,_inner,inn,&denom,&err);
+
+  if ((E<0.)||(fabs(Fc)<tol)) { *res = denom; printf("E<0: %.5e\n", denom); return;}
+  //else if (fabs(Fc)<tol) {*res=1.; return;} // no mods.
+
+  if (Fc<0) { printf("FCEG still to be implemented\n"); return; }
+
   double out[3] = {SQR(Qs_M),SQR(Qsp_M),fabs(Fc)*as};
   integrator(0.,x_max,_outer,out,&res_outer,&err);
-  out[0] = 0.; // to calc. Zpc w/o FCEL
-  integrator(1e-8,1.,_inner,out,&denom,&err);
-  *res = res_outer/denom;
-
+  *res = res_outer; printf("E>0: %.5e\n", res_outer);
+  
 }
 
-/*--------------------------------------------------------------------*/
-// colour-bilities: g, g -> q, \bar{q}
-/*
-double rho_1(double xi, double *Fc) { *Fc = 0;
-  double xibar = 1.-xi,
-         denom = SQR(Nc*xi) + SQR(Nc*xibar) - 1.;
-  return 1./denom;
-}
+double Z_sum_( double E, 
+               double (*dsig)(double,double), double (*phi)(double),
+               void *params) {
 
-double rho_8(double xi, double *Fc) { *Fc = Ca;
-  double dud   = 0;
-  return 1.-rho_1(xi,&dud);
-}
-// */
-
-//#define IRREPS 2
-//rho reps[IRREPS] = {&rho_1,&rho_8};
-
-double Z_sum_(double E, void *params) {
   double Fc, temp, prob, res=.0,
          xi  = ((double *)params)[1];
 
   for (int i=0;i<IRREPS;i++) {
     prob = reps[i](xi,&Fc);
-    //printf("%g\n",Fc);
-    Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
+    printf("%g\n",Fc);
+    Zpc_FCEL(Fc,E,dsig,phiN,params,&res);
     res+= prob*temp;
   };
 
@@ -129,116 +118,118 @@ double Z_sum_(double E, void *params) {
 
 int main() {
 
-  double R1, R2, R3;
-  double E = 1., xi=.5, prob;
-  double res;
-  double Fc;
-
-  double alpha_s = .5, ootp = 1./(2.*M_PI);
-  // starting params
-  double params[3] = {alpha_s*ootp,0.001,sqrt(L_eff(14.5)/L_p)};
-  printf ("Qs/Qsp = % .10f\n", params[2] );
-
-  char *prefix=(char*)"FCEL_scaling2";
-  char  suffix[20];
-  char  filename[50];
-
-  // filename
-  strcpy(filename,prefix);
-  sprintf(suffix,".dat");
-  strcat(filename,suffix);
-  out=fopen(filename,"w");
-  fprintf(out,"# FCEL (w/scaling), alpha=%g\n",alpha_s);
-  fprintf(out,"# columns: Qs/M, F(kappa=.3), F(kappa=.4), F(kappa=.5)\n");
-
-  double frac;
-  g=.01;
-
-  while (g<4.) {
-    //frac = params[1]/.64;
-    frac = g/4.;
-
-    R1=0.; R2=0.; R3=0.;
-
-    params[1]=.3;
-    //prob = rho_1(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R1+= prob*res;
-    //prob = rho_8(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R1 = res;
-    R1 = Z_sum_(E,params);
-
-    //g=2.5;
-    params[1]=.4;
-    //prob = rho_1(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R2+= prob*res;
-    //prob = rho_8(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R2 = res;
-    R2 = Z_sum_(E,params);
-
-    //g=3.;
-    params[1]=.5;
-    //prob = rho_1(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R3+= prob*res;
-    //prob = rho_8(xi,&Fc);
-    //Zpc_FCEL(Fc,E,dsig_1,phiN,params,&res);
-    //R3 = res;
-    R3 = Z_sum_(E,params);
-
-    printf(" Qs/M = %.5e , [%2.2f%]\n", params[1] , 100.*frac); 
-    fprintf( out, "%.8e   %.8e   %.8e   %.8e\n", g, R1, R2, R3 );
-    g += .1;
-  }
-
-  //double A=14.5;
-  //printf ("% .10f   % .10f\n", A, L_eff(A) );
-  printf(" Saved to file ["); printf(filename); printf("]\n"); fclose(out);
+  Z_scan_g();
+  //r_scan_E(dsig_1,phi_H3a);
 
   return 0;
 }
 
 /*--------------------------------------------------------------------*/
 
-void R_scan_pT(double y) {
-  int N_pT;
-  double R, Rmin, Rmax, pT, pT_min, pT_max, step;
+void r_scan_E( double (*dsig)(double,double),
+               double (*phi)(double)           ) { // SOMETHING WRONG!!
+  int N_E;
+  double E, Emin, Emax, step,
+         Z_orig, Z_fcel;
 
-  char *prefix=(char*)"out/RpA_";
+  double kT = 0., x2 = 1e-7, xi = .5, qhat = .07, m = 1.5;
+  double alpha_s = .5, ootp = 1./(2.*M_PI);
+
+  char *prefix=(char*)"r_FCEL_E";
   char  suffix[20];
   char  filename[50];
 
   // filename
   strcpy(filename,prefix);
-  sprintf(suffix,"{rs=%.2f,y=%.1f}.dat",1.,y);
+  sprintf(suffix,"{kT=%.2f,x2=%.1f}.dat",kT,x2);
   strcat(filename,suffix);
   out=fopen(filename,"w");
-  //fprintf(out,"# R_pA, A=%.1f, alpha=%g\n",A,alpha_s);
-  //fprintf(out,"# columns: pT, R_ave, R_min, R_max\n");
+  fprintf(out,"# FCEL (w/scaling), alpha=%g\n",alpha_s);
+  fprintf(out,"# columns: E/GeV r (1)\n");
+
+
+  double res_outer, err;
+
+  double as   = alpha_s*ootp,
+         M2   = ( SQR(kT) + SQR(m) )/( xi*(1.-xi) ),
+         //Q2p  = Qs2(L_p,qhat,x2);
+         Q2A  = Qs2(L_eff(14.5),qhat,x2);
+
+  // starting params
+  //
+  double params[3] = {as,sqrt(Q2A/M2),sqrt(L_eff(14.5)/L_p)};
 
   // Here are some parameters that can be changed:
-  N_pT=50; 
-  pT_min=1.;
-  pT_max=10.;
+  N_E=50; 
+  Emin=1e5;
+  Emax=1e8;
   // don't change anything after that.
 
-  step=(pT_max-pT_min)/((double) N_pT-1);
-  pT=pT_min;
+  double frac;
+  E = Emin;
+  step=pow(Emax/Emin,1./(double)(N_E-1));
 
-  printf(" Settings: y=%g, with pT_min=%g, pT_max=%g\n",y,pT_min,pT_max); 
+  for (int i=0;i<N_E;i++) { 
+    frac = (double)i/(double)(N_E-1);
+
+    printf(" E = %.5e , [%2.2f%]\n", E, 100.*frac); 
+
+    Z_orig = Z_sum_(-E,dsig,phi,params);
+    Z_fcel = Z_sum_(+E,dsig,phi,params);
+
+    printf( "%.8e   %.8e   %.8e\n", E, Z_orig, Z_fcel);
+    fprintf( out, "%.8e   %.8e   %.8e   %.8e\n", E, Z_orig, Z_fcel, Z_fcel/Z_orig);
+
+    E *= step;
+  }
+
+  printf(" Saved to file ["); printf(filename); printf("]\n"); fclose(out);
+}
+
+void Z_scan_g() {
+  double gmin, gmax, dg;
+
+  double R1, R2, R3;
+  double E = 1., xi=.5;
+  double alpha_s = .5, ootp = 1./(2.*M_PI);
+  // starting params
+  //
+  double params[3] = {alpha_s*ootp,0.01,sqrt(L_eff(14.5)/L_p)};
+
+
+  char *prefix=(char*)"FCEL_scaling_";
+  char  suffix[20];
+  char  filename[50];
+
+  // filename
+  strcpy(filename,prefix);
+  sprintf(suffix,"{E=%.2f,xi=%.1f}.dat",E,xi);
+  strcat(filename,suffix);
+  out=fopen(filename,"w");
+  fprintf(out,"# FCEL (w/scaling), alpha=%g\n",alpha_s);
+  fprintf(out,"# columns: Qs/M, F(kappa=.3), F(kappa=.4), F(kappa=.5)\n");
+
+  // Here are some parameters that can be changed:
+  gmin=.01;
+  gmax=4.;
+  dg=.1;
+  // don't change anything after that.
+
+  g=gmin;
+
+  printf(" Settings: Qs/Qsp=%g, with g_min=%g, g_max=%g\n",params[2],gmin,gmax); 
   double frac;
 
-  for (int i=0; i<N_pT; i++) {
-    frac = (double)i/(double)(N_pT-1);
+  while (g<gmax) {
+    frac = g/4.;
 
-    //R_limits(pT,y,&R,&Rmin,&Rmax);
+    params[1]=.3; R1 = Z_sum_(E,dsig_1,phiN,params);
+    params[1]=.4; R2 = Z_sum_(E,dsig_1,phiN,params);
+    params[1]=.5; R3 = Z_sum_(E,dsig_1,phiN,params);
 
-    printf(" pT = %.5e , [%2.2f%]\n", y , 100.*frac); 
-    fprintf( out, "%.8e   %.8e   %.8e   %.8e\n", pT, R, Rmin, Rmax );
-    pT += step;
+    printf(" gamma = %.5e , [%2.2f%]\n", g , 100.*frac); 
+    fprintf( out, "%.8e   %.8e   %.8e   %.8e\n", g, R1, R2, R3 );
+    g += dg;
   }
 
   printf(" Saved to file ["); printf(filename); printf("]\n"); fclose(out);
