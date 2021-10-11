175931b2a9454f49ed57bce581e687d9d5fbc48a
greg jackson <greg@wam.co.za>
2021-10-11 16:49:33 -0700

    Added qqbar -> qqbar probas

--- fcel.h
+++ fcel.h
@@ -97,12 +97,22 @@ double gg_qq_8(double xi, double *Fc) { *Fc = Ca;
   return 1.-gg_qq_1(xi,&dud);
 }
 // */
+//
+// colour-bilities: q, q -> Q, \bar{Q}
+
+double qq_qq_8(double xi, double *Fc) { *Fc = Ca;
+  return 1.;
+}
+
+// */
+
 
 //#define IRREPS 2
 rho reps_gg_gg[3] = {&gg_gg_1,&gg_gg_8,&gg_gg_27};
 rho reps_qg_qg[3] = {&qg_qg_3,&qg_qg_6,&qg_qg_15};
 rho reps_gq_qg[3] = {&gq_qg_3,&gq_qg_6,&gq_qg_15};
 rho reps_gg_qq[2] = {&gg_qq_1,&gg_qq_8};
+rho reps_qq_qq[1] = {&qq_qq_8};
 rho *reps;
 char reaction[10];
 int IRREPS;
@@ -113,6 +123,7 @@ void channel(int r) {
     case 2 : strcpy(reaction, "{qg;qg}"); reps = reps_qg_qg; IRREPS = 3; break;
     case 3 : strcpy(reaction, "{gq;qg}"); reps = reps_gq_qg; IRREPS = 3; break;
     case 4 : strcpy(reaction, "{gg;qq}"); reps = reps_gg_qq; IRREPS = 2; break;
+    case 5 : strcpy(reaction, "{qq;qq}"); reps = reps_qq_qq; IRREPS = 1; break;
     default: reps = NULL;
   }
   printf("Switched channel to %d\n",r);
