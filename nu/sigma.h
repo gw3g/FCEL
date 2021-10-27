e2ecfbeb49332c62b453620440ddcd4b70f15448
Greg J <jackson@itp.unibe.ch>
2021-10-27 13:29:32 -0700

    Martin's param. added

--- nu/sigma.h
+++ nu/sigma.h
@@ -13,3 +13,10 @@ double dsig_2(double E, double xF) { // MRST-1998
   if (xF>1.) {return 1e-12;}
   return pow(1.-xF,6.)*(1.-2.8*sqrt(xF)+2.8*xF)/xF;
 }
+
+double dsig_3(double E, double xF) { // Martin 2003
+  double A  = 140. + pow( 11.*log( E/100. ), 1.65);
+  double n  = 7.6 + .025*log(E/10000.);
+  double be = .05 - .016*log(E/10000.);
+  return A*pow(xF,be-1.)*pow(1.-pow(xF,1.2),n);
+}
